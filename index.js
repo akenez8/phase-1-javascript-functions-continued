4c6bfc5e8dba607aaf7c2c3932c494b61220bbd9
peter <+>
2022-08-30 09:28:26 -0400

    commit tree f96a8be50d32614a3263c6bd1cc4f237850a17ce

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(value) {
+    if (value == null) {
+        return `This Saturday, I want to roller-skate!`;
+    }
+    else if (value !== null) {
+        return `This Saturday, I want to bathe my dog!`;
+    }
+}
+
+function mondayWork(arg) {
+    if (arg == null) {
+        return `This Monday, I will go to the office.`;
+    }
+    else if (arg == `work from home`) {
+        return "This Monday, I will work from home.";
+    }
+}
+
+
+function wrapAdjective(emphatic){
+    return function (emphText) {
+        return `You are ${emphatic}${emphText}${emphatic}!`;
+    }
+}
